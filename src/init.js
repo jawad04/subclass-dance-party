317751b5fcd79dfd8234cb217e5378e503e71079
jawad04 <muhammadijawad10@gmail.com>
2018-09-25 12:17:50 -0700

    updated init dancer location to be inside of the body

--- src/init.js
+++ src/init.js
@@ -23,8 +23,8 @@ $(document).ready(function() {
 
     // make a dancer with a random position
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $("body").height() * Math.random() - 20,
+      $("body").width() * Math.random() - 20,
       Math.random() * 2000
     );
     $('body').append(dancer.$node);
